7203ef7cdc036d55052013e8737eca7e08f530ff
Cerveau Eric <eric.cerveau@hotmail.fr>
2018-09-11 18:08:03 +0200

    recognizing url.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var auth = require('./auth.json');
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
+const urlRegex = require('url-regex');
 const regUrl = new RegExp('/([-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?)|(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$|(:[0-9]{4})$)){4}|([0-9a-f]){1,4}(:([0-9a-f]){1,4}){7}(:[0-9]{4})?/gi');
-const isAWord = new RegExp('[a-zA-Z]{1,}');
+const isAWord = new RegExp('[a-zA-Z]*');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (regUrl.test(msg.content)){
+  if (urlRegex().test(msg.content)){
     msg.reply(" espèce de gros con à foutre des url sans cheuvrons.");
   } else if (msg.content === 'ping') {
     msg.reply('Pong!');
